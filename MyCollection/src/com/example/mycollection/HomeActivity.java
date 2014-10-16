62ac2c92dde69f6a8dcbacecfc8835a357814db5
OliveChao <chaoolive@gmail.com>
2014-10-16 13:09:02 +0300

    modified

--- MyCollection/src/com/example/mycollection/HomeActivity.java
+++ MyCollection/src/com/example/mycollection/HomeActivity.java
@@ -8,7 +8,7 @@ public class HomeActivity extends ActionBarActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_home);
+		setContentView(R.layout.home);
 
 		
 	}
