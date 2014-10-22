e754bbe5532851d524557723c8ae0fe2a3e4dce9
OliveChao <chaoolive@gmail.com>
2014-10-22 11:20:29 +0300

    Added main screen java file

---
+++ TakaApplication/src/com/example/takaapplication/MainScreenActivity.java
@@ -0,0 +1,13 @@
+package com.example.takaapplication;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class MainScreenActivity extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main_screen);
+	}
+}
