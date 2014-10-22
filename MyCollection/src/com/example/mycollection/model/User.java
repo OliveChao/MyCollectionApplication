60cf702fbfcb730275ebb3f0de85ab3f9c80d917
OliveChao <chaoolive@gmail.com>
2014-10-22 13:08:45 +0300

    modified

---
+++ MyCollection/src/com/example/mycollection/model/User.java
@@ -0,0 +1,37 @@
+package com.example.mycollection.model;
+
+/**
+ * @author olive chao
+ * @date 16th October,2014 Thursday
+ *
+ */
+
+public class User {
+	
+	String firstName;
+	String lastName;
+	String email;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		//initialization
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+
+}
